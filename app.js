5704653e5eb01a83125527fdcc0f3aa86e4b22b6
DidierrM <didiermateus@hotmail.com>
2024-06-27 11:13:13 -0500

    prueba 27 junio 2024

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function asignarTextoElemento(elemento, texto) {
     elementoHTML.innerHTML = texto;
     return;
 }
-
+// probando
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
     
